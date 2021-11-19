f3ae3916de4d1cdb7659024677d19bea98c308df
santiagomartin20 <santiagomartin20@users.noreply.github.com>
2021-11-19 10:42:00 -0300

    Update ApiPostoApplication.java
    Troca de letras

--- SpringBootPosto/src/main/java/br/com/apiposto/ApiPostoApplication.java
+++ SpringBootPosto/src/main/java/br/com/apiposto/ApiPostoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApipostoApplication {
+public class ApiPostoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApipostoApplication.class, args);
+		SpringApplication.run(ApiPostoApplication.class, args);
 	}
 
 }
