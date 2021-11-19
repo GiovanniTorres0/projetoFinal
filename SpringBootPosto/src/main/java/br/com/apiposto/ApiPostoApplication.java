574bb0bea932021a1c6b5bf352312b96dcfed73f
GiovanniTorres0 <73203847+GiovanniTorres0@users.noreply.github.com>
2021-11-18 22:15:10 -0300

    Base

--- SpringBootPosto/src/main/java/br/com/apiposto/ApiPostoApplication.java
+++ SpringBootPosto/src/main/java/br/com/apiposto/ApiPostoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiPostoApplication {
+public class ApipostoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiPostoApplication.class, args);
+		SpringApplication.run(ApipostoApplication.class, args);
 	}
 
 }
