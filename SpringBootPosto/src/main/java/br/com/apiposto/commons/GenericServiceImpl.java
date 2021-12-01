6e65b07c75d34d6722626dde0985b954aaf4eb3a
GiovanniTorres0 <73203847+GiovanniTorres0@users.noreply.github.com>
2021-12-01 19:45:40 -0300

    Adição handler na lógica

--- SpringBootPosto/src/main/java/br/com/apiposto/commons/GenericServiceImpl.java
+++ SpringBootPosto/src/main/java/br/com/apiposto/commons/GenericServiceImpl.java
@@ -7,34 +7,61 @@ import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import br.com.apiposto.config.ErroDeValidacionHandler;
 
 @Service
 public abstract class GenericServiceImpl<T, ID extends Serializable> implements GenericServiceAPI<T, ID> {
 
+	private MethodArgumentNotValidException exception;
+	private ErroDeValidacionHandler ev = new ErroDeValidacionHandler();
+
 	@Override
 	public T save(T entity) {
-		return getDao().save(entity);
+		try {
+			return getDao().save(entity);
+		} catch (Exception e) {
+			ev.handler(exception);
+			return null;
+		}
 	}
 
 	@Override
 	public void delete(ID id) {
-		getDao().deleteById(id);
+		try {
+			getDao().deleteById(id);
+		} catch (Exception e) {
+			ev.handler(exception);
+		}
+
 	}
 
 	@Override
 	public T get(ID id) {
+
 		Optional<T> obj = getDao().findById(id);
 		if (obj.isPresent()) {
 			return obj.get();
+		} else {
+			ev.handler(exception);
+			return null;
 		}
-		return null;
+
 	}
 
 	@Override
 	public List<T> getAll() {
-		List<T> returnList = new ArrayList<>();
-		getDao().findAll().forEach(obj -> returnList.add(obj));
-		return returnList;
+		try {
+
+			List<T> returnList = new ArrayList<>();
+			getDao().findAll().forEach(obj -> returnList.add(obj));
+			return returnList;
+
+		} catch (Exception e) {
+			ev.handler(exception);
+			return null;
+		}
 	}
 
 	public abstract CrudRepository<T, ID> getDao();
