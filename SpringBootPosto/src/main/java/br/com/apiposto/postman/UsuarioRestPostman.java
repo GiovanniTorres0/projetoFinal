113fed5088fc20eaa0416f2b341add517f4c718b
GiovanniTorres0 <73203847+GiovanniTorres0@users.noreply.github.com>
2021-12-01 20:28:58 -0300

    Inserção novo controller

--- SpringBootPosto/src/main/java/br/com/apiposto/postman/UsuarioRestPostman.java
+++ SpringBootPosto/src/main/java/br/com/apiposto/postman/UsuarioRestPostman.java
@@ -1,4 +1,4 @@
-package br.com.apiposto.restcontroller;
+package br.com.apiposto.postman;
 
 import java.io.IOException;
 import java.util.List;
@@ -7,9 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,8 +22,8 @@ import br.com.apiposto.service.UsuarioServiceApi;
 import br.com.apiposto.service.imp.GeolocalizacaoService;
 
 @RestController
-@RequestMapping("/usuario/api")
-public class UsuarioRestController {
+@RequestMapping("/usuario")
+public class UsuarioRestPostman {
 
 	@Autowired
 	private UsuarioServiceApi usuarioServiceAPI;
@@ -42,7 +42,7 @@ public class UsuarioRestController {
 	}
 
 	@PostMapping(value = "/save")
-	public ResponseEntity<Usuario> save(@ModelAttribute Usuario usuario) {
+	public ResponseEntity<Usuario> save(@RequestBody Usuario usuario) {
 
 		usuario.getUbicacion().setId((long) 1);
 
@@ -91,3 +91,6 @@ public class UsuarioRestController {
 	}
 
 }
+
+	
+
