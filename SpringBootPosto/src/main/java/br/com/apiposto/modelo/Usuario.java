d18750b866f5cbd09679e5a0943740d093e9590a
santiagomartin20 <santiagomartin20@users.noreply.github.com>
2021-12-02 08:52:25 -0300

    Update Security

--- SpringBootPosto/src/main/java/br/com/apiposto/modelo/Usuario.java
+++ SpringBootPosto/src/main/java/br/com/apiposto/modelo/Usuario.java
@@ -1,38 +1,64 @@
 package br.com.apiposto.modelo;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
+
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Document(collection = "usuarios")
-public class Usuario {
+public class Usuario implements UserDetails {
 
+	private static final long serialVersionUID = 1L;
 	@Id
 	private Long id;
 	private String nome;
+	private String email;
+	private String senha;
 	@DBRef
 	private Ubicacion ubicacion;
-
+	@DBRef
+	private List<Perfil> perfils =new ArrayList<>();
+	
 	public Usuario() {
 	}
-	
 
-	public Usuario(String nome, Ubicacion ubicacion) {
+	public Usuario(Long id, String nome, String email, String senha, Ubicacion ubicacion) {
+		this.id = id;
 		this.nome = nome;
+		this.email = email;
+		this.senha = senha;
 		this.ubicacion = ubicacion;
 	}
 
-	
-	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
 	public Ubicacion getUbicacion() {
 		return ubicacion;
 	}
 
-
 	public void setUbicacion(Ubicacion ubicacion) {
 		this.ubicacion = ubicacion;
 	}
-
 
 	public Long getId() {
 		return id;
@@ -48,6 +74,41 @@ public class Usuario {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return this.perfils;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 
 }
